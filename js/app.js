49d2be424bdd02645018719520c6f061bfbd8937
AraceliGS <aragutarras@gmail.com>
2018-02-07 12:51:08 -0500

    añadiendo plantilla base

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* VERSIÓN 1 */
+$(document).ready(function() {
+
+});
+
+
+/* VERSIÓN 2 */
+function begin() {
+
+}
+$(document).ready(begin);
---
+++ js/tooltip-popover.js
@@ -0,0 +1,9 @@
+// Función de bootstrap para popover
+$(function() {
+  $('[data-toggle="popover"]').popover();
+});
+
+// Función de bootstrap para tooltip
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
