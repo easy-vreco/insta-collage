710c927ae072574913fb848dfc63b1036c329df3
AraceliGS <aragutarras@gmail.com>
2018-02-08 02:00:52 -0500

    añadiendo avances

--- js/app.js
+++ js/app.js
@@ -6,17 +6,31 @@ $(document).ready(function() {
   $inputEmail.on('input', function() {
     console.log($(this).val());
     const PATTERNEMAIL = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
-    let $result = PATTERNEMAIL.test($(this).val());
-    console.log($result);
+    let $resultEmail = PATTERNEMAIL.test($(this).val());
+    console.log($resultEmail);
   });
 
   $inputPassword.on('input', function(event) {
     console.log($(this).val());
+    /* const PATTERNPASSWORD = /^[\d]{6,}$/;
+    const PATTERNNUMBERS = /[0-9]/g;
+    let $resultPassword1 = PATTERNPASSWORD.test($(this).val());
+    // let $resultPassword2 = PATTERNNUMBERS.test($(this).val());
+    console.log($resultPassword1);
+    // console.log($resultPassword2);*/
+    let $arrayOfCharacters = $(this).val().join();
+    $arrayOfCharacters.sort(compare);
+    var compare = function(st, nd) {
+      return a - b;
+    };
+    console.log(compare());
     if ($(this).val().length >= 6) {
-      debugger;
-      $('#register-form-group').addClass('has-success');
+      /* No se añaden los estilos - Comprobar*/ 
+      alert('Cumple');
+      $inputPassword.addClass('has-success');
     } else {
-      $('#register-form-group').addClass('has-error');
+      $inputPassword.addClass('has-error');
+      alert('No cumple');
     }
   });
 });
